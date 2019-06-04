f05a3c0a5f7492eaedd6624fb5ea3b740a8d9c64
BanlyTong <banly2602@gmail.com>
2019-06-04 13:14:09 +0700

    implement add todo function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todo: "",
+      todos: ["Learning ReactJS", "Learning Redux", "Learning JS"]
+    }
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({todos: [...this.state.todos, this.state.todo ], todo: ""});
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({todo: e.target.value })
+  }
+
+  render() {
+    const todoList = this.state.todos.map((todo, index) => {
+      return <li key={index}>{todo}</li>
+    });
+    return (
+      <div className="App">
+        <form>
+          <input type="text" value={this.state.todo} onChange={ this.handleChange } placeholder="Todo What?" />
+          <input type="submit" value="Add" onClick={this.handleSubmit}/>
+        </form>
+        <ul>
+          {todoList}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
